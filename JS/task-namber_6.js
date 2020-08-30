1981b7a8cf3d87058f463b5e1834f5c6b55c2023
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-08-30 22:22:09 +0300

    Спроби

--- JS/task-namber_6.js
+++ JS/task-namber_6.js
@@ -8,7 +8,79 @@
 // После того как пользователь прекратил ввод нажав кнопку Cancel, показать alert со строкой 
 // 'Общая сумма чисел равна [сумма]'.
 // bell Делать проверку того, что пользователь ввел именно число, а не произвольный набор символов, не нужно.
-// Если хочешь, в случае некорректного ввода, показывай alert с текстом 'Было введено не число, попробуйте еще раз', при этом результат prompt плюсовать к общей сумме не нужно, после чего снова пользователю предлагается ввести число в prompt.
+// Если хочешь, в случае некорректного ввода, показывай alert с текстом 'Было введено не число, попробуйте еще раз', 
+// при этом результат prompt плюсовать к общей сумме не нужно, после чего снова пользователю предлагается ввести число в prompt.
 
-// let input;
+
+
+
+let total = 0;
+let inpu03.334     t = prompt('Пожалуйста введите число');
+
+ffddssd
+       
+if (input > 0) {
+    console.log(input);    
+}else if (input === null) {
+    alert(`Общая сумма чисел равна ${total += input}`);
+}else {
+    alert('Было введено не число, попробуйте еще раз');
+}  
+
+
+
+input = Number(input);
+ total += input;
+   console.log('total: ', total); 
+    
+// for (let total = 0; total >= 100; total += input) {
+//     console.log(total);
+
+// const input = prompt('Пожалуйста введите число';
 // let total = 0;
+
+// 
+// console.log(total);
+
+
+
+
+
+// // let input = '';
+
+
+
+// console.log('Ввели пароль: ', input);
+
+
+
+
+
+
+// do {
+//     input = prompt('Пожалуйста введите число');
+    
+//     total += input;
+//     console.log('total: ', total);
+
+//     input = Number(input);
+//     console.log(input);
+
+   
+//   } 
+//   while (input >= 0 && input);
+
+    
+
+// let message = prompt('Пожалуйста введите число');
+// input = 
+// console.log(input);
+
+
+
+
+
+
+// for (let total = 0; total > input; input += 1) {
+//     prompt('Пожалуйста введите число');
+// }
