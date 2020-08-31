4163ae7a2a24f0e237bb412a5383f5326bbab884
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-08-31 21:43:35 +0300

    Виконав ДЗ першого модуля

--- JS/task-namber_6.js
+++ JS/task-namber_6.js
@@ -12,75 +12,21 @@
 // при этом результат prompt плюсовать к общей сумме не нужно, после чего снова пользователю предлагается ввести число в prompt.
 
 
-
-
 let total = 0;
-let inpu03.334     t = prompt('Пожалуйста введите число');
+let input;
 
-ffddssd
-       
-if (input > 0) {
-    console.log(input);    
-}else if (input === null) {
-    alert(`Общая сумма чисел равна ${total += input}`);
+do {
+    input = prompt('Пожалуйста введите число', '0');
+    console.log(Number(input)); 
+
+if (isNaN(input)) {
+      alert('Было введено не число, попробуйте еще раз');   
 }else {
-    alert('Было введено не число, попробуйте еще раз');
-}  
+     total += input;
+     console.log('total: ', total);
+}
+} while (input !== null);
 
+  
 
-
-input = Number(input);
- total += input;
-   console.log('total: ', total); 
-    
-// for (let total = 0; total >= 100; total += input) {
-//     console.log(total);
-
-// const input = prompt('Пожалуйста введите число';
-// let total = 0;
-
-// 
-// console.log(total);
-
-
-
-
-
-// // let input = '';
-
-
-
-// console.log('Ввели пароль: ', input);
-
-
-
-
-
-
-// do {
-//     input = prompt('Пожалуйста введите число');
-    
-//     total += input;
-//     console.log('total: ', total);
-
-//     input = Number(input);
-//     console.log(input);
-
-   
-//   } 
-//   while (input >= 0 && input);
-
-    
-
-// let message = prompt('Пожалуйста введите число');
-// input = 
-// console.log(input);
-
-
-
-
-
-
-// for (let total = 0; total > input; input += 1) {
-//     prompt('Пожалуйста введите число');
-// }
+alert(`Общая сумма чисел равна ${total}`);
