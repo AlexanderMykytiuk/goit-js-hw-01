dd1cc328e28cbd51e1cfc9f689791f59ee3d16e9
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-09-09 19:21:06 +0300

    Виконав задачу №2

--- JS/task-namber_2.js
+++ JS/task-namber_2.js
@@ -4,31 +4,43 @@
 // строку (в строке будут только слова и пробелы) и цену гравировки одного слова, 
 // и возвращающую цену гравировки всех слов в строке.
 
-// const calculateEngravingPrice = function(message, pricePerWord) {
-//   // твой код
-// };
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(
-//   calculateEngravingPrice(
-//     'Proin sociis natoque et magnis parturient montes mus',
-//     10,
-//   ),
-// ); // 80
 
-// console.log(
-//   calculateEngravingPrice(
-//     'Proin sociis natoque et magnis parturient montes mus',
-//     20,
-//   ),
-// ); // 160
+const calculateEngravingPrice = function(message, pricePerWord) {
+  let total = 0;
+ 
+  for(let i = 0; i < message.length; i += 1) {
 
-// console.log(
-//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-// ); // 240
+       message = message.split(' ');
+       console.table(message);
 
-// console.log(
-//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-// ); // 120
+       total = message.length * pricePerWord;
+
+    return `Total: ${total}`
+  }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
