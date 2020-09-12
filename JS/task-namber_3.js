bada5672be445ec4a6e1527f7e7543104e881d8b
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-09-12 20:04:52 +0300

    Майже виконав задачу №3

--- JS/task-namber_3.js
+++ JS/task-namber_3.js
@@ -2,15 +2,36 @@
 // Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку
 //  (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
 
-// const findLongestWord = function(string) {
-//   // твой код
-// };
+const findLongestWord = function(string) {
+    
+    let longestWord = string.split(' ');
+    
+        console.table(longestWord);
 
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+   for(const value of longestWord) {
+    console.log(value);
 
-// console.log(findLongestWord('Google do a roll')); // 'Google'
+    if (value.length  > longestWord.length) {
+        // longestWord = value; 
+      
+    }
+    
+    // return value
+    
+  }
+  
+};
 
-// console.log(findLongestWord('May the force be with you')); // 'force'
+// let longestWord = value[0];
+
+// .length
+
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
