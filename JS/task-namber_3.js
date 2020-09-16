173823385d89a57f4b9ab8dfa0122c849212ca82
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-09-16 16:14:01 +0300

    Виконав задачу №3

--- JS/task-namber_3.js
+++ JS/task-namber_3.js
@@ -25,30 +25,54 @@
 // // let longestWord = value[0];
 
 // // .length
-function findLongestWord (string = "") {
-  // Write code under this line
-     //  smallValue = string[0]; 
- let smallValue = string.split(' ');
+// function findLongestWord (string = "") {
+//   // Write code under this line
+//      //  smallValue = string[0]; 
+//  let smallValue = string.split(' ');
    
   
-  for(const value of string) {
+//   for(const value of string) {
 
     
-    if(value > smallValue) {
-      smallValue = value;
-     }
+//     if(value > smallValue) {
+//       smallValue = value;
+//      }
    
+//   }
+//      return smallValue
+// };
+
+   
+
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
+
+function findLongestWord (string = "") {
+  // Write code under this line
+  string = string.split(' ');
+  let largeValue = string[0];
+  
+  for (const value of string) {
+   
+    if (value.length > largeValue.length) {
+      largeValue = value;
+    }
+    
   }
-     return smallValue
+  return largeValue;
 };
 
-   
+//console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+//console.log(findLongestWord('Google do a roll'));
+// 'Google'
 
-console.log(findLongestWord('Google do a roll')); // 'Google'
-
-console.log(findLongestWord('May the force be with you')); // 'force'
+//console.log(findLongestWord('May the force be with you'));
+// 'force'
