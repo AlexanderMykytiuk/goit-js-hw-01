28d1faae1a6953c8da02ce8098e77ace124631fd
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-09-16 16:51:48 +0300

    Виконав Задачу №4

--- JS/task-namber_4.js
+++ JS/task-namber_4.js
@@ -7,24 +7,47 @@
 
 
 
-const formatString = function(string) {
+// const formatString = function(string) {
 
-    const stringLength = string > string.slice(0, 40) ? string.concat('. . .') : string;
+//     const stringLength = string > string.slice(0, 40) ? string.concat('. . .') : string;
      
-    return stringLength
-};
+//     return stringLength
+// };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-// вернется оригинальная строка
+// /*
+//  * Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// // вернется оригинальная строка
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-// вернется форматированная строка
+// console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// // вернется форматированная строка
 
-console.log(formatString('Curabitur ligula sapien.'));
-// вернется оригинальная строка
+// console.log(formatString('Curabitur ligula sapien.'));
+// // вернется оригинальная строка
 
-console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
-// вернется форматированная строка
+// console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
+// // вернется форматированная строка
+
+function formatString (string, maxLength = 40) {
+    // Write code under this line
+    //return string.length
+    for (const value of string) {
+     if (string.length > maxLength) {
+          string = string.slice(0, 40).concat('...'); 
+     }
+      if (maxLength === 30) {
+          string = string.slice(0, 30).concat('...');
+      }
+    }
+    return string
+  }
+  
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+  // 'Curabitur ligula sapien, tincidunt non.'
+  
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+  // 'Vestibulum facilisis, purus nec pulvinar...'
+  
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+  // 'Vestibulum facilisis, purus ne...'
