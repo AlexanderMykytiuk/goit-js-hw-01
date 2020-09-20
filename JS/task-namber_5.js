c637b2b0e558f8737a3bc9e873d92e5cc5951091
Oleksandr Mykytiuk <alexandermykytiuk@gmail.com>
2020-09-20 17:56:24 +0300

    Виконав 6 задач

--- JS/task-namber_5.js
+++ JS/task-namber_5.js
@@ -4,17 +4,31 @@
 // Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. 
 // Слова в строке могут быть в произвольном регистре.
 
-const checkForSpam = function(message) {
-  // твой код
+'use strict';
+const checkForSpam = function (str) {
+  let arrStr = str.split(' ');
+
+  for (let i = 0; i < arrStr.length; i += 1) {
+    let word = arrStr[i].toLowerCase;
+
+    if (arrStr[i].toLowerCase === 'spam' || arrStr[i].toLowerCase === 'sale') {
+      return true;
+    }
+
+    return false;
+  }
+
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(checkForSpam('Latest technology news')); // false
+// Вызовы функции для проверки
 
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log( checkForSpam('Latest technology news') ); // false
 
-console.log(checkForSpam('Get best sale offers now!')); // true
+console.log( checkForSpam('JavaScript weekly newsletter') ); // false
 
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log( checkForSpam('Get best sale offers now!') ); // true
+
+console.log( checkForSpam('[SPAM] How to earn fast money?') ); // true
+
+
+//Як тут використати includes()????
